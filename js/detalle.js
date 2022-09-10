92f82b628342f9421fa8f26e390927052d46686e
floralonso <floralonso@gmail.com>
2022-09-10 09:42:12 -0300

    ajuste la página detalle considerando la clase de Emi y ahora funciona

--- js/detalle.js
+++ js/detalle.js
@@ -5,12 +5,12 @@ console.log(window.location.search);
 const parametroCodigo = new URLSearchParams(window.location.search);
 
 console.log(parametroCodigo.get('codigo'))
-//buscar en el arreglo de peliculas cuál tiene el mismo código de mi parametro
+//buscar en el arreglo de partituras cuál tiene el mismo código de mi parametro
 
-let listaPeliculas = JSON.parse(localStorage.getItem('Libros')) || [];
-let peliBuscada = listaPeliculas.find((pelicula) =>{return pelicula.codigo === parametroCodigo.get('codigo')});
+let listaPartituras = JSON.parse(localStorage.getItem('Libros')) || [];
+let partiBuscada = listaPartituras.find((partitura) =>{return partitura.codigo === parametroCodigo.get('codigo')});
 
-console.log(peliBuscada)
+console.log(partiBuscada)
 
 
 let detalle = document.querySelector('#seccionDetalle');
@@ -18,13 +18,13 @@ detalle.innerHTML = `
         <div class="card mb-3">
           <div class="row g-0">
             <div class="col-md-4">
-              <img src="${peliBuscada.imagen}" class="img-fluid rounded-start" alt="Thor: Love and Thunder">
+              <img src="${partiBuscada.imagen}" class="img-fluid rounded-start" alt="Thor: Love and Thunder">
             </div>
             <div class="col-md-8">
               <div class="card-body">
-                <h5 class="card-title">${peliBuscada.titulo}</h5>
-                <p class="card-text">${peliBuscada.descripcion}</p>
-                <p class="card-text">${peliBuscada.genero} <span class="badge rounded-pill bg-info">Ciencia Ficcion</span></p>
+                <h5 class="card-title">${partiBuscada.titulo}</h5>
+                <p class="card-text">${partiBuscada.descripcion}</p>
+                <p class="card-text">${partiBuscada.categoria} <span class="badge rounded-pill bg-info">$${partiBuscada.precio}</span></p>
                 <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
               </div>
             </div>
