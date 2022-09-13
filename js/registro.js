190e0c923224acd8c4acb30e4a1bd32b45b5ac0c
floralonso <floralonso@gmail.com>
2022-09-13 08:02:51 -0300

    no pude generar el registro de usuario

--- js/registro.js
+++ js/registro.js
@@ -1,32 +1,69 @@
+var objPeople = [
+	{
+		username: 'floralonso',
+		password: 'contador'
+	},
+	{
+		username: 'matt',
+		password: 'password88'
+	},
+	{
+		username: 'chris',
+		password: 'password3'
+	}
+]
 
-//EVENTOS PARA VALIDACIONES
-regUsuario.addEventListener('blur', ()=>{cantidadCaracteresUsuario(regUsuario)});
-contrasena.addEventListener('blur', ()=>{validarconstrasena(contrasena)});
+// login functionality
+function login() {
+	// retreive data from username and store in username variable
+	var username = document.getElementById('username').value
+	// retreive data from password and store in password variable
+	var password = document.getElementById('password').value
 
-
-//VALIDACIONES
-
-function cantidadCaracteresUsuario(input){
-    if( input.value.trim().length >= 3 && input.value.trim().length <=40 ){
-        console.log('dato valido');
-        input.className = 'form-control is-valid';
-        return true;
-    }else{
-        console.log('dato invalido');
-        input.className = 'form-control is-invalid';
-        return false;
-    }
+	// loop through all the user pbjects and confrim if the username and password are correct
+	for(var i = 0; i < objPeople.length; i++) {
+		// check to 
+		if(username == objPeople[i].username && password == objPeople[i].password) {
+			console.log(username + ' is logged in!!!')
+            location.replace("/index.html")
+			// stop the statement if result is found true - this was a return in the video, break is best practice here
+			break
+		} else {
+			// error if username and password don't match
+			console.log('incorrect username or password')
+		}
+	}
 }
 
-function validarconstrasena(input){
-          let expReg = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/;
-        if(expReg.test(input.value)){
-            input.className = 'form-control is-valid';
-            return true;
-        }else{
-            input.className = 'form-control is-invalid';
-            return false;
-        }
-    }
-
-    //To check a password between 6 to 20 characters which contain at least one numeric digit, one uppercase and one lowercase letter
+// register functionality
+function registerUser() {
+	// store new users username
+	var registerUsername = document.getElementById('newUsername').value
+	// store new users password
+	var registerPassword = document.getElementById('newPassword').value
+	// store new user data in an object
+	var newUser = {
+		username: registerUsername,
+		password: registerPassword
+	}
+	// loop throught people array to see if the username is taken, or password to short
+	for(var i = 0; i < objPeople.length; i++) {
+		// check if new username is equal to any already created usernames
+		if(registerUser == objPeople[i].username) {
+			// alert user that the username is take
+			alert('That username is alreat in user, please choose another')
+			// stop the statement if result is found true
+			break
+		// check if new password is 8 characters or more
+		} else if (registerPassword.length < 8) {
+			// alert user that the password is to short
+			alert('That is to short, include 8 or more characters')
+			// stop the statement if result is found true
+			break
+		}
+	}
+	// push new object to the people array
+	objPeople.push(newUser)
+	// console the updated people array
+	console.log(objPeople)
+}
